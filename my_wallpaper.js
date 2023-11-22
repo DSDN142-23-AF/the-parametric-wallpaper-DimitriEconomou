61b0eb346719bba2df12b55f2aec720d28682fe8
DimitriEconomou <151587165+DimitriEconomou@users.noreply.github.com>
2023-11-22 23:49:27 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,17 +1,17 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
+//cottagecore background variabkes
+let horozontalShift=20 //horozontal shift can only do in 20's 
+let verticalShift=20 //20,40,80,100
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
@@ -19,5 +19,42 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  
+  //cottagecore background
+  strokeWeight(8)
+  stroke(0,0,0,100,5)
+ 
+  line(verticalShift,0,verticalShift,200) //vertical left to right
+  line(2*verticalShift,0,2*verticalShift,200)
+  line(3*verticalShift,0,3*verticalShift,200)
+  line(4*verticalShift,0,4*verticalShift,200)
+  line(5*verticalShift,0,5*verticalShift,200)
+  line(6*verticalShift,0,6*verticalShift,200)
+  line(7*verticalShift,0,7*verticalShift,200)
+  line(8*verticalShift,0,8*verticalShift,200)
+  line(9*verticalShift,0,9*verticalShift,200)
+  line(10*verticalShift,0,10*verticalShift,200)
+
+  
+  line(0,horozontalShift,200,horozontalShift) //horozontal lines top to bottom
+  line(0,2*horozontalShift,200,2*horozontalShift)
+  line(0,3*horozontalShift,200,3*horozontalShift)
+  line(0,4*horozontalShift,200,4*horozontalShift)
+  line(0,5*horozontalShift,200,5*horozontalShift)
+  line(0,6*horozontalShift,200,6*horozontalShift)
+  line(0,7*horozontalShift,200,7*horozontalShift)
+  line(0,8*horozontalShift,200,8*horozontalShift)
+  line(0,9*horozontalShift,200,9*horozontalShift)
+  line(0,10*horozontalShift,200,10*horozontalShift)
+
+
+
+
+
+
+
+  
 }
+
+
+
